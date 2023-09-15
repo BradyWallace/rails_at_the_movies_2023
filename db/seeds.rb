a852b4aca488595c23a0b32dbd7bd77bbca7c40a
Brady Wallace <bwallace2@academic.rrc.ca>
2023-09-15 14:21:26 -0500

    Modified seed script to pull movie data

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,9 @@ require "csv"
 Movie.delete_all
 ProductionCompany.delete_all
 
+ActiveRecord::Base.connection.execute("DELETE FROM sqlite_sequence WHERE name='production_companies';")
+ActiveRecord::Base.connection.execute("DELETE FROM sqlite_sequence WHERE name='movies';")
+
 # open top_movies.csv
 filename = Rails.root.join("db/top_movies.csv")
 
@@ -15,12 +18,21 @@ movies = CSV.parse(csv_data, headers: true, encoding: "utf-8")
 movies.each do |m|
   production_company = ProductionCompany.find_or_create_by(name: m["production_company"])
 
-  if production_company && production_company.valid?
+  if production_company&.valid?
     # create the movie
+    movie = production_company.movies.create(
+      title:        m["original_title"],
+      year:         m["year"],
+      duration:     m["duration"],
+      description:  m["description"],
+      average_vote: m["avg_vote"]
+    )
+    puts "Invalid movie #{m['original_title']}" unless movie&.valid?
   else
     puts "Invalid production company #{m['production_company']} for movie #{m['original_title']}"
   end
   puts m["original_title"]
 end
 
-puts "Created #{ProductionCompany.count} Production Companies"
+puts "Created #{ProductionCompany.count} Production Companies."
+puts "Created #{Movie.count} Movies."
