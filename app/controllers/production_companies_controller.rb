435aabb68281bee341640794aad9d41f5d10ca0e
Brady Wallace <bwallace2@academic.rrc.ca>
2023-09-19 15:50:17 -0500

    Views, bulma, methods

--- app/controllers/production_companies_controller.rb
+++ app/controllers/production_companies_controller.rb
@@ -1,7 +1,13 @@
 class ProductionCompaniesController < ApplicationController
   def index
+    # all production companies
+    # @production_companies = ProductionCompany.all
+
+    @production_companies = ProductionCompany.ordered_by_movies
   end
 
   def show
+    # a specific production company
+    @production_company = ProductionCompany.find(params[:id])
   end
 end
